59f5bfedfa4f5fdd7bd7902047347185aeb2bd86
ERICMILLER12 <>
2022-03-09 17:14:11 +0000

    ws1

--- Explore1.cpp
+++ Explore1.cpp
@@ -9,28 +9,30 @@ int main() {
     // Remove the comment below
     // a.x = b.x;
     // Why doesn't it work?
-
+    
 
     // First instance of Point, named a - starts at default 0.0, 0.0
-    //Point b(4.5, 7.8);
+    Point b(4.5, 7.8);
 
     //cout << "Point a: x = " << a.getX() << ", y = " << a.getY() << endl;
     //cout << "Point b: x = " << b.getX() << ", y = " << b.getY() << endl;
 
-    //a.setX(53);
+   // a.setX(53);
     //a.setY(51);
     //cout << "Point a: x = " << a.getX() << ", y = " << a.getY() << endl;
 
+    a.print();
+    b.print();
     //cout << "Point a:";
     //a.print();
     //cout << "Point b:";
     //b.print(); 
 
     // Look! the equal operator still works!
-    // a = b;
+    a = b;
     //cout << "Point a:";
     //a.print();
-
+     cout << a.calcDistance(b);
     // a.setPoints(33.9, 1072.8);
     //cout << "Point a:";
     //a.print();
--- Point.h
+++ Point.h
@@ -1,7 +1,17 @@
 class Point{
     public:
         Point();
+        Point(double xVal, double yVal);
+        double calcDistance(Point other);
+/*
+        double getX();
+        double getY();
+        void setX(double xVal);
+        void setY(double yVal);
+*/
+void print();
     private:
         double x;
         double y;
+      
 };
